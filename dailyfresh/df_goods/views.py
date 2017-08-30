271923dccd1274a7d5ca91202dd5bb88e53a5c10
kethener <874433434@qq.com>
2017-08-30 14:24:23 +0800

    首页完善

--- dailyfresh/df_goods/views.py
+++ dailyfresh/df_goods/views.py
@@ -20,7 +20,29 @@ def home_list_page(request):
     fruits_new = Goods.objects.get_goods_list_by_type(goods_type_id=FRUIT, limit=3, sort='new')
     fruits = Goods.objects_logic.get_goods_list_by_type(goods_type_id=FRUIT, limit=4)
 
-    context = {'fruits_new': fruits_new, 'fruits': fruits}
+    seafood_new = Goods.objects.get_goods_list_by_type(goods_type_id=SEAFOOD, limit=3, sort='new')
+    seafood = Goods.objects_logic.get_goods_list_by_type(goods_type_id=SEAFOOD, limit=4)
+
+    meat_new = Goods.objects.get_goods_list_by_type(goods_type_id=MEAT, limit=3, sort="new")
+    meat = Goods.objects_logic.get_goods_list_by_type(goods_type_id=MEAT, limit=4)
+
+    eggs_new = Goods.objects.get_goods_list_by_type(goods_type_id=EGGS, limit=3, sort='new')
+    eggs = Goods.objects_logic.get_goods_list_by_type(goods_type_id=EGGS, limit=4)
+
+    vegetables_new = Goods.objects.get_goods_list_by_type(goods_type_id=VEGETABLES, limit=3, sort='new')
+    vegetables = Goods.objects_logic.get_goods_list_by_type(goods_type_id=VEGETABLES, limit=4)
+
+    frozen_new = Goods.objects.get_goods_list_by_type(goods_type_id=FROZEN, limit=3, sort='new')
+    frozen = Goods.objects_logic.get_goods_list_by_type(goods_type_id=FROZEN, limit=4)
+    # 2.组织上下文
+    context = {'fruits_new': fruits_new, 'fruits': fruits,
+               'seafood_new': seafood_new, 'seafood': seafood,
+               'meat_new': meat_new, 'meat': meat,
+               'eggs_new': eggs_new, 'eggs': eggs,
+               'vegetables_new': vegetables_new, 'vegetables': vegetables,
+               'frozen_new': frozen_new, 'frozen': frozen
+               }
+    # 3.调用模板文件
     return render(request, 'index.html', context)
 
 
@@ -50,17 +72,23 @@ def goods_list(request, goods_type_id, page_index):
     # 取第page_index页的内容　有上一页:has_previous 有下一页:has_next 当前页:number
     goods_li = paginator.page(int(page_index))
 
+    # 控制页码列表，大于５页的时候只显示５页内容
     if len(pages) > 5:
+        # 判断如果当前页是前三页，那么就只显示前５页
         if goods_li.number <= 3:
             pages = pages[:5]
+        # 如果当前页是后三页，那么就只显示后５页
         elif goods_li.number >= (len(pages)-2):
             pages = pages[-5:]
+        # 如果当前页不在上述范围内，那么当前页就显示在中间位置
         else:
             start = goods_li.number-3
             end = goods_li.number+2
             pages = pages[start:end]
+    # 当页码列表不到５页的时候，就全部显示
     else:
         pages = pages
+
     # 获取商品的新品信息
     goods_new_li = Goods.objects_logic.get_goods_list_by_type(goods_type_id=goods_type_id, limit=2, sort='new')
     context = {'goods_li': goods_li, 'goods_new_li': goods_new_li,
